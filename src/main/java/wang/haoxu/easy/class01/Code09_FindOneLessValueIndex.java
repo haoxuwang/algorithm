0c1883e69631d337c357aabf23abd3e030e696b7
William Wang <william.wang@inmobi.com>
2021-08-05 18:55:41 +0800

    test

--- src/main/java/wang/haoxu/easy/class01/Code09_FindOneLessValueIndex.java
+++ src/main/java/wang/haoxu/easy/class01/Code09_FindOneLessValueIndex.java
@@ -2,6 +2,14 @@ package wang.haoxu.easy.class01;
 
 public class Code09_FindOneLessValueIndex {
 
+	/**
+	 * 局部最小值
+	 * 如果arr[0]<arr[1] ,arr[0]为局部最小值
+	 * 如果arr[arr.length-1]<arr[arr.length-2] ,arr[arr.length-1]为局部最小值
+	 * 否则局部最小值一定在中间 并且至少有一个
+	 * @param arr
+	 * @return
+	 */
 	public static int getLessIndex(int[] arr) {
 		if (arr == null || arr.length == 0) {
 			return -1; // no exist
@@ -18,10 +26,34 @@ public class Code09_FindOneLessValueIndex {
 		while (left < right) {
 			mid = (left + right) / 2;
 			if (arr[mid] > arr[mid - 1]) {
+				// 左边至少有一个
+				/**
+				 *			。（中点）							。
+				 *		。		。							。
+				 *	。				。					。
+				 *  								。
+				 *								。（中点）
+				 */
 				right = mid - 1;
 			} else if (arr[mid] > arr[mid + 1]) {
+				// 右边至少有一个
+				/**
+				 * 	。
+				 * 		。
+				 * 			。
+				 * 				。
+				 * 					。（中点）
+				 */
 				left = mid + 1;
 			} else {
+				/**
+				 *
+				 * 	。				。
+				 * 		。		。
+				 * 			。（中点）        			。	。	。（中点）	。	。
+				 *
+				 *
+				 */
 				return mid;
 			}
 		}
